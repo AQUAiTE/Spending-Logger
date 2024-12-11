2918431fe27695f1303eb7dca28f2c965aafe2b8
AQUAiTE <francisiwnlschool@gmail.com>
2024-12-10 23:43:07 -0800

    Removed tests until setup done, added Spotless formatting and DotEnv

--- Backend (Spring)/spendinglogger/spendinglogger/src/main/java/org/aquaite/spendinglogger/SpendingloggerApplication.java
+++ Backend (Spring)/spendinglogger/spendinglogger/src/main/java/org/aquaite/spendinglogger/SpendingloggerApplication.java
@@ -1,14 +1,14 @@
 package org.aquaite.spendinglogger;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpendingloggerApplication {
 
-	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().directory("src/main/resources").load();
-		SpringApplication.run(SpendingloggerApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    Dotenv dotenv = Dotenv.configure().directory("src/main/resources").load();
+    SpringApplication.run(SpendingloggerApplication.class, args);
+  }
 }
